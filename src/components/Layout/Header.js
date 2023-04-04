8d3ee59e7cf784770360e62ef2127a1736cd293e
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-04-04 15:45:44 +0530

    set-up

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import mealImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+export default function Header(props) {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="food" />
+      </div>
+    </>
+  );
+}
